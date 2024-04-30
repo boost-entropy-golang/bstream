d05d5d5d6c932d01083f1f89d0ecf56de0babeb5
Matthieu Vachon <matt@streamingfast.io>
2024-04-30 15:40:02 -0400

    Removed completely `PassthroughTransform` and related functionalities

--- transform/interfaces.go
+++ transform/interfaces.go
@@ -1,27 +1,15 @@
 package transform
 
 import (
-	"context"
 	"time"
 
 	"github.com/streamingfast/bstream"
 	pbbstream "github.com/streamingfast/bstream/pb/sf/bstream/v1"
-	"github.com/streamingfast/bstream/stream"
-	pbfirehose "github.com/streamingfast/pbgo/sf/firehose/v2"
-	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/known/anypb"
 )
 
 type Transform interface {
 	String() string
-}
-
-type StreamGetter func(ctx context.Context, handler bstream.Handler, request *pbfirehose.Request, logger *zap.Logger) (*stream.Stream, error)
-type StreamOutput func(*bstream.Cursor, BlockMetadata, *anypb.Any) error
-
-type PassthroughTransform interface {
-	Run(ctx context.Context, req *pbfirehose.Request, getStream StreamGetter, output StreamOutput) error
 }
 
 type PreprocessTransform interface {
